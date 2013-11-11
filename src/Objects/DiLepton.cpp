656f0e06b7915d0a3bedd3fb796315ff163ddee4
Nik Berry <eepgnnb@pion2.brunel.ac.uk>
2013-11-11 15:31:21 +0000

    couple of additions. Ratio plots not working for values < 0

--- src/Objects/DiLepton.cpp
+++ src/Objects/DiLepton.cpp
@@ -22,26 +22,81 @@ DiLepton::~DiLepton() {
 void DiLepton::allPlots(AllSamples samples){
 
 	//  Variable::Variable(TString name_temp, TString xTitle_temp, double minX_temp, double maxX_temp, int rebinFact_temp)
-	Variable pt_lead("LeadLepton_Pt", "lead #mu p_{T}", 0, 300, 5);
+	Variable pt_lead("LeadLepton_Pt", "lead lepton p_{T}", 0, 300, 5);
+	Variable pt_second("SecondLepton_Pt", "second lepton p_{T}", 0, 300, 5); 
+        Variable eta_lead("LeadLepton_Eta", "lead lepton #eta", -3, 3, 5);
+        Variable eta_second("SecondLepton_Eta", "second lepton #eta", -3, 3, 5);
+	Variable abseta_lead("LeadLepton_AbsEta", "lead lepton |#eta|", 0, 3, 5);
+        Variable abseta_second("SecondLepton_AbsEta", "second lepton |#eta|", 0, 3, 5);
+        Variable phi_lead("LeadLepton_Phi", "lead lepton #phi", -4, 4, 5);
+        Variable phi_second("SecondLepton_Phi", "second lepton #phi", -4, 4, 5);
+	Variable dileptonmass("diLepton_Mass", "M_{ll}", 0, 400, 5);
 
 	setSelectionAndChannel("TTbarDiLeptonAnalysis", "MuMu");
 	savePlot(samples, pt_lead);
-
+        savePlot(samples, pt_second);
+        savePlot(samples, eta_lead);
+        savePlot(samples, eta_second);
+	savePlot(samples, abseta_lead);
+        savePlot(samples, abseta_second);
+        savePlot(samples, phi_lead);
+        savePlot(samples, phi_second);
+        savePlot(samples, dileptonmass);
+	
 	setChannel("EE");
 	savePlot(samples, pt_lead);
+        savePlot(samples, pt_second);
+        savePlot(samples, eta_lead);
+        savePlot(samples, eta_second);
+        savePlot(samples, abseta_lead);
+        savePlot(samples, abseta_second);
+        savePlot(samples, phi_lead);
+        savePlot(samples, phi_second);
+        savePlot(samples, dileptonmass);
 
 	setChannel("EMu");
 	savePlot(samples, pt_lead);
+        savePlot(samples, pt_second);
+        savePlot(samples, eta_lead);
+        savePlot(samples, eta_second);
+        savePlot(samples, abseta_lead);
+        savePlot(samples, abseta_second);
+        savePlot(samples, phi_lead);
+        savePlot(samples, phi_second);
+        savePlot(samples, dileptonmass);
 
 	setSelectionAndChannel("TTbarPhotonAnalysis", "MuMu");
 	savePlot(samples, pt_lead);
+        savePlot(samples, pt_second);
+        savePlot(samples, eta_lead);
+        savePlot(samples, eta_second);
+        savePlot(samples, abseta_lead);
+        savePlot(samples, abseta_second);
+        savePlot(samples, phi_lead);
+        savePlot(samples, phi_second);
+        savePlot(samples, dileptonmass);
 
 	setChannel("EE");
 	savePlot(samples, pt_lead);
+        savePlot(samples, pt_second);
+        savePlot(samples, eta_lead);
+        savePlot(samples, eta_second);
+        savePlot(samples, abseta_lead);
+        savePlot(samples, abseta_second);
+        savePlot(samples, phi_lead);
+        savePlot(samples, phi_second);
+        savePlot(samples, dileptonmass);
 
 	setChannel("EMu");
 	savePlot(samples, pt_lead);
-
+        savePlot(samples, pt_second);
+        savePlot(samples, eta_lead);
+        savePlot(samples, eta_second);
+        savePlot(samples, abseta_lead);
+        savePlot(samples, abseta_second);
+        savePlot(samples, phi_lead);
+        savePlot(samples, phi_second);
+        savePlot(samples, dileptonmass);
 }
 
 } /* namespace std */
--- src/Objects/Photons.cpp
+++ src/Objects/Photons.cpp
@@ -21,18 +21,47 @@ Photons::~Photons() {
 void Photons::allPlots(AllSamples samples){
 
 	Variable photon_pt("Photon_ET", "Photon E_{T} [GeV]", 0, 150, 2);
-	Variable photon_eta("Photon_Eta", "Photon #eta", -3., 3., 2);
+	Variable photon_eta("Photon_Eta", "Photon #eta", -3., 3., 10);
+	Variable photon_abseta("Photon_AbsEta", "Photon |#eta|", 0., 3., 10);
+	Variable photon_sigmaietaieta_barrel("Photon_sigma_ietaieta_barrel", "#sigmai#etai#eta(barrel)", 0., 0.1, 10);
+	Variable photon_sigmaietaieta_endcap("Photon_sigma_ietaieta_endcap", "#sigmai#etai#eta(endcap)", 0., 0.1, 12);
+	Variable photon_rhocorrchargediso_barrel("Photon_RhoCorrectedPFChargedHadronIso_barrel", "RhoCorrPFChargedHadronIso", -15., 100., 20);
+	Variable photon_rhocorrchargediso_endcap("Photon_RhoCorrectedPFChargedHadronIso_endcap", "RhoCorrPFChargedHadronIso", -15., 100., 20);	
+	Variable photon_numberofphotons("Number_Of_Photons", "N(#gamma)", -0.5, 6.5, 1);
 
-	setPhotonSel("AllPhotons");
+
 	setSelectionAndChannel("TTbarDiLeptonAnalysis", "MuMu");
+	setPhotonSel("AllPhotons");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+	savePlot(samples, photon_abseta);
+	savePlot(samples, photon_sigmaietaieta_barrel);
+	savePlot(samples, photon_sigmaietaieta_endcap);
+	savePlot(samples, photon_rhocorrchargediso_barrel);
+	savePlot(samples, photon_rhocorrchargediso_endcap);
+	savePlot(samples, photon_numberofphotons);
+	
+	photon_sigmaietaieta_endcap.rebinFact *= 2;
+	
 	setChannel("EE");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+	savePlot(samples, photon_abseta);
+	savePlot(samples, photon_sigmaietaieta_barrel);
+	savePlot(samples, photon_sigmaietaieta_endcap);
+	savePlot(samples, photon_rhocorrchargediso_barrel);
+	savePlot(samples, photon_rhocorrchargediso_endcap);
+	savePlot(samples, photon_numberofphotons);
+	
 	setChannel("EMu");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+	savePlot(samples, photon_abseta);
+	savePlot(samples, photon_sigmaietaieta_barrel);
+	savePlot(samples, photon_sigmaietaieta_endcap);
+	savePlot(samples, photon_rhocorrchargediso_barrel);
+	savePlot(samples, photon_rhocorrchargediso_endcap);
+	savePlot(samples, photon_numberofphotons);
 
 	photon_pt.rebinFact *= 2;
 	photon_eta.rebinFact *= 2;
@@ -41,23 +70,67 @@ void Photons::allPlots(AllSamples samples){
 	setPhotonSel("SignalPhotons");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+	savePlot(samples, photon_abseta);
+        savePlot(samples, photon_sigmaietaieta_barrel);
+        savePlot(samples, photon_sigmaietaieta_endcap);
+        savePlot(samples, photon_rhocorrchargediso_barrel);
+        savePlot(samples, photon_rhocorrchargediso_endcap);
+        savePlot(samples, photon_numberofphotons);
+	
+	photon_abseta.rebinFact *= 2;
+	photon_sigmaietaieta_barrel.rebinFact *= 2;
+//	photon_sigmaietaieta_endcap.rebinFact *= 2;
+
 	setChannel("EE");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+        savePlot(samples, photon_abseta);
+        savePlot(samples, photon_sigmaietaieta_barrel);
+        savePlot(samples, photon_sigmaietaieta_endcap);
+        savePlot(samples, photon_rhocorrchargediso_barrel);
+        savePlot(samples, photon_rhocorrchargediso_endcap);
+        savePlot(samples, photon_numberofphotons);
+
 	setChannel("EMu");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+        savePlot(samples, photon_abseta);
+        savePlot(samples, photon_sigmaietaieta_barrel);
+        savePlot(samples, photon_sigmaietaieta_endcap);
+        savePlot(samples, photon_rhocorrchargediso_barrel);
+        savePlot(samples, photon_rhocorrchargediso_endcap);
+        savePlot(samples, photon_numberofphotons);
 
 	setPhotonSel("NminusOnePhotons");
 	setChannel("MuMu");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+        savePlot(samples, photon_abseta);
+        savePlot(samples, photon_sigmaietaieta_barrel);
+        savePlot(samples, photon_sigmaietaieta_endcap);
+        savePlot(samples, photon_rhocorrchargediso_barrel);
+        savePlot(samples, photon_rhocorrchargediso_endcap);
+//        savePlot(samples, photon_numberofphotons);
+
 	setChannel("EE");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+        savePlot(samples, photon_abseta);
+        savePlot(samples, photon_sigmaietaieta_barrel);
+        savePlot(samples, photon_sigmaietaieta_endcap);
+        savePlot(samples, photon_rhocorrchargediso_barrel);
+        savePlot(samples, photon_rhocorrchargediso_endcap);
+//        savePlot(samples, photon_numberofphotons);
+
 	setChannel("EMu");
 	savePlot(samples, photon_pt);
 	savePlot(samples, photon_eta);
+        savePlot(samples, photon_abseta);
+        savePlot(samples, photon_sigmaietaieta_barrel);
+        savePlot(samples, photon_sigmaietaieta_endcap);
+        savePlot(samples, photon_rhocorrchargediso_barrel);
+        savePlot(samples, photon_rhocorrchargediso_endcap);
+//        savePlot(samples, photon_numberofphotons);
 }
 
 void Photons::setPhotonSel(TString photon_sel){
